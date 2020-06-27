785366ec52ee015360d848095d0a9e6af81f1d61
Arjun Agarwal <arjunagarwal899@gmail.com>
2020-06-27 20:29:12 +0530

    Installed react-router-dom and set it up in App.js. Installed Ant Design.

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+
+import "antd/dist/antd.css";
 import './App.css';
 
+import {BrowserRouter as Router} from "react-router-dom";
+import BaseRouter from "./routes";
+
 function App() {
-  return (
-    <div className="App">
-        Hello group members!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+
+                <BaseRouter/>
+
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/gui/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+
+const BaseRouter = () => {
+    return (
+        <React.Fragment>
+            {/*<Route exact path='/' component={}/>*/}
+        </React.Fragment>
+    );
+};
+
+export default BaseRouter;
