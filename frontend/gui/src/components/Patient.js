8b02d532d12b8883463affbd23c5fefe2ed130e2
Arjun Agarwal <31124126+arjunagarwal899@users.noreply.github.com>
2020-07-06 15:02:58 +0530

    Minor changes

--- frontend/gui/src/components/Patient.js
+++ frontend/gui/src/components/Patient.js
@@ -1,20 +1,33 @@
 import React from 'react';
 
-import {DatePicker, Form, Input, Select} from "antd";
+import {DatePicker, Form, Input, Radio, Select} from "antd";
 
 const Patient = () => {
 	return (
 		<Form>
 			<Form.Item label="Patient ID:">
-				<Input placeholder="Enter patient ID" />
+				<Input placeholder="Enter patient ID"/>
 			</Form.Item>
 
 			<Form.Item label="Title:">
-				<Input placeholder="Enter title" />
+				<Select>
+					<Select.Option value="Mr">Mr.</Select.Option>
+					<Select.Option value="Mas">Master</Select.Option>
+					<Select.Option value="Mrs">Mrs.</Select.Option>
+					<Select.Option value="Ms">Ms.</Select.Option>
+					<Select.Option value="Dr">Dr.</Select.Option>
+				</Select>
+				{/*<Radio.Group>*/}
+				{/*	<Radio value="Mr">Mr.</Radio>*/}
+				{/*	<Radio value="Mas">Master</Radio>*/}
+				{/*	<Radio value="Mrs">Mrs.</Radio>*/}
+				{/*	<Radio value="Ms">Ms.</Radio>*/}
+				{/*	<Radio value="Dr">Dr.</Radio>*/}
+				{/*</Radio.Group>*/}
 			</Form.Item>
 
 			<Form.Item label="Patient Name:">
-				<Input placeholder="Enter patient name" />
+				<Input placeholder="Enter patient name"/>
 			</Form.Item>
 
 			<Form.Item label="Date of Birth:">
@@ -22,16 +35,18 @@ const Patient = () => {
 			</Form.Item>
 
 			<Form.Item label="Age:">
-				<Input placeholder="Age" />
+				<Input placeholder="Age"/>
 			</Form.Item>
 
 			<Form.Item label="Gender:">
-				<Select defaultValue="Unknown">
-					<Select.Option value="male">Male</Select.Option>
-					<Select.Option value="female">Female</Select.Option>
-					<Select.Option value="unknown">Unknown</Select.Option>
-				</Select>
+				<Radio.Group>
+					<Radio value="male">Male</Radio>
+					<Radio value="female">Female</Radio>
+					<Radio value="unknown">Unknown</Radio>
+				</Radio.Group>
 			</Form.Item>
+
+			<Input/>
 		</Form>
 	);
 };
